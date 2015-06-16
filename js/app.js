bf8c7424736e87f8f89b09bff8781017848658c1
Jacob Adelgren <jadelgren2012@gmail.com>
2015-06-15 23:02:02 -0600

    metric tire to inches is working

--- js/app.js
+++ js/app.js
@@ -12,6 +12,10 @@ sampleApp.config(['$routeProvider',
     templateUrl: 'views/gearRatioTable.html',
     controller: 'gearRatioTableController'
       }).
+      when('/MetricTireInches', {
+    templateUrl: 'views/metricTireInches.html',
+    controller: 'metricTireInchesController'
+      }).
       otherwise({
     redirectTo: '/IdealGearRatio'
       });
@@ -20,13 +24,8 @@ sampleApp.config(['$routeProvider',
 
 sampleApp.controller('idealGearRatioController', function($scope) {
     
-/*    $scope.message = "";
-    $scope.originalRatio = "";
-    $scope.originalTireSize = "";
-    $scope.newTireSize = "";
-    $scope.newRatio = "";*/
-
     $scope.calculateNewRatio = function() {
+      if($scope.originalRatio > 0 && $scope.originalTireSize > 0 && $scope.newTireSize > 0) {
         $scope.newRatio = ($scope.newTireSize / $scope.originalTireSize) * $scope.originalRatio;
         if(isNaN($scope.newRatio) || null === $scope.newRatio) {
           $scope.message = "An error occurred. Please check your inputs!"
@@ -35,7 +34,7 @@ sampleApp.controller('idealGearRatioController', function($scope) {
           $scope.newRatio = $scope.newRatio.toFixed(2); // convert to 2 decimal places
           $scope.message = "Your ideal gear ratio is " + $scope.newRatio;
         }
-        
+      }
     }
 });
 
@@ -44,4 +43,21 @@ sampleApp.controller('gearRatioTableController', function($scope) {
 
     $scope.message = 'boop';
 
+});
+
+sampleApp.controller('metricTireInchesController', function($scope) {
+    $scope.calculateTireSize = function() {
+      if($scope.ratio > 0 && $scope.width > 0 && $scope.rim > 0) {
+        $scope.tireSize = ($scope.width * ($scope.ratio / 100) * 2 / 25.4) + +$scope.rim;
+        if(isNaN($scope.tireSize) || null === $scope.tireSize) {
+          $scope.message = "An error occurred. Please check your inputs!"
+          $scope.tireSize = ""
+        } else {
+          $scope.tireSize = $scope.tireSize.toFixed(2); // convert to 2 decimal places
+          $scope.message = "The tire is " + $scope.tireSize +"\" tall";
+        }
+    } else {
+      $scope.message = ""
+    }
+    }
 });
